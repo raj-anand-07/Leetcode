919f974818789ba5ece4367706996cdd3bc7de8f
Anand Raj <raj.anand240406@gmail.com>
2026-09-06 14:40:20 +0530

    Time: 30 ms (58.87%) | Memory: 44.4 MB (20.60%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,91 @@
+class Solution {
+public:
+    int solve(string s, string t, int i, int j) {
+        if(j < 0) {
+            return 1;
+        }
+
+        if(i < 0) {
+            return 0;
+        }
+
+        int take = 0;
+        int leave = solve(s, t, i - 1, j);
+
+        if(s[i] == t[j]) {
+            take = solve(s, t, i - 1, j - 1);
+
+            return (take + leave);
+        }
+
+        return leave;
+    }
+
+    int solveMem(string s, string t, int i, int j, vector<vector<int>>& dp) {
+        if(j < 0) {
+            return 1;
+        }
+
+        if(i < 0) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int take = 0;
+        int leave = solveMem(s, t, i - 1, j, dp);
+
+        if(s[i] == t[j]) {
+            take = solveMem(s, t, i - 1, j - 1, dp);
+
+            return dp[i][j] = (take + leave);
+        }
+
+        return dp[i][j] = leave;
+    }
+
+    int solveTab(string s, string t, int n, int m) {
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+
+        for(int i = 0; i <= n; i++) {
+
+            dp[i][0] = 1;
+
+        }
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= m; j++) {
+
+                if(s[i - 1] == t[j - 1]) {
+
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+
+                }
+
+                else {
+
+                    dp[i][j] = dp[i -1][j];
+
+                }
+            }
+        }
+
+        return (int)dp[n][m];
+    }
+
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        // return solve(s, t, n - 1, m - 1);
+
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // return solveMem(s, t, n - 1, m - 1, dp);
+
+        return solveTab(s, t, n, m);
+
+    }
+};
