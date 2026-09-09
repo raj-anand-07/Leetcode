f261edb93f4b4c8cf90b0150d516eb24ea7d5f28
Anand Raj <raj.anand240406@gmail.com>
2026-09-09 14:46:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -2,39 +2,11 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans = 0;
+        long long base = 1000;
 
-        if(n < 1000) {
-            return ans;
-        }
-
-        else if(n < 1e6) {
-            ans += (n - 999);
-        }
-
-        else if(n < 1e9) {
-            ans += (n - 999);
-            ans += (n - 999999);
-        }
-
-        else if(n < 1e12) {
-            ans += (n - 999);
-            ans += (n - 999999);
-            ans += (n - 999999999);
-        }
-
-        else if(n < 1e15) {
-            ans += (n - 999);
-            ans += (n - 999999);
-            ans += (n - 999999999);
-            ans += (n - 999999999999);
-        }
-
-        else {
-            ans += (n - 999);
-            ans += (n - 999999);
-            ans += (n - 999999999);
-            ans += (n - 999999999999);
-            ans += (n - 999999999999999);
+        while(base <= n) {
+            ans += (n - base + 1);
+            base *= 1000;
         }
 
         return ans;
